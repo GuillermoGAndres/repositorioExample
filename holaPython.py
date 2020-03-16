d8ec32c91f2bfca0ce5669732d83e01449764726
GuillermoGAndres <47510430+GuillermoGAndres@users.noreply.github.com>
2020-03-15 18:11:19 -0600

    other example with python for me
    Test how work github

---
+++ holaPython.py
@@ -0,0 +1,13 @@
+
+print("Hola mundo")
+
+
+def despedir():
+    print("Adios :(")
+
+
+for x in range(1, 5):
+    print(x," Muchos holas :D")
+    despedir()
+
+print("*******jojojojojo**********")
